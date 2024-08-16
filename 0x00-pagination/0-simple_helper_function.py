2180700e345f35310af722e5d0dcabdbf3e116fb
AnasMouak <anasmouak1111@gmail.com>
2024-08-16 18:07:54 +0000

    Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def index_range(page, page_size):
+    if page < 1 or page_size < 1:
+        return []
+
+    start = (page - 1) * page_size
+    end = start + page_size
+
+    return (start, end)
